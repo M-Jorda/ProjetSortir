29d9def5480275131717a7d1df9dad0252558c1e
Melchior Gibson <jorda.j.fr@gmail.com>
2024-03-05 18:49:04 +0100

    Possibilité de supprimer un campus

--- src/Controller/Utilisateur/CampusController.php
+++ src/Controller/Utilisateur/CampusController.php
@@ -9,10 +9,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/campus', name: 'campus-')]
 class CampusController extends AbstractController
 {
 
-    #[Route('/campus/add', name: 'campus-add', methods: ['GET','POST'])]
+    #[Route('/add', name: 'add', methods: ['GET','POST'])]
     public function addCampus(Request $request, EntityManagerInterface $em) {
         $campus = new Campus();
         $campuss = $em->getRepository(Campus::class)->findAll();
@@ -32,5 +33,20 @@ class CampusController extends AbstractController
             'campusForm' => $campusForm,
             'campuss' => $campuss
         ]);
+    }
+
+    #[Route('delete/{id}', name: 'delete', methods: ['GET', 'POST'])]
+    public function delete(int $id, EntityManagerInterface $em) {
+        $campus = $em->getRepository(Campus::class)->find($id);
+
+        if (!$campus) {
+            throw $this->createNotFoundException('Ce campus n\'existe pas');
+        } else {
+            $em->remove($campus);
+            $em->flush();
+
+            $this->addFlash('success', 'Ce campus é été correctement supprimé');
+        }
+        return $this->redirectToRoute('campus-add');
     }
 }
